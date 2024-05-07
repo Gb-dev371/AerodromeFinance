07820d99de39ca7deb1c8171660d67564801c223
Gb-dev371 <gbdev371@gmail.com>
2024-05-07 12:36:03 -0300

    bribe data added

--- bribes.py
+++ bribes.py
@@ -1,78 +1,14 @@
-import requests
-import os
-from dotenv import load_dotenv
-from web3 import Web3
-from utils import get_block_number_by_timestamp, get_contract_abi
 from ScanApi.scan_api import ScanApi
 from ScanApi.utils.functions import timestamp_to_date, convert_date_to_timestamp
-
-load_dotenv(override=True)
-NODE_URL = os.getenv('NODE_URL')
-API_KEY_BASE_SCAN = os.getenv('API_KEY_BASE_SCAN')
-base_node = Web3(Web3.HTTPProvider(NODE_URL))
+from web3 import Web3
 
 
 def get_pool_name(scan_api:ScanApi, pool_contract_address):
     pool_abi = scan_api.get_contract_abi(pool_contract_address)
     pool_contract = scan_api.get_contract(pool_contract_address, pool_abi)
     return pool_contract.functions.name().call()
-
-
-''' VOTES '''
-def get_pool_votes_in_a_timestamp(pool, voter_contract, timestamp):
-    """_summary_
-
-    Args:
-        pool (str): _description_
-        timestamp (int): _description_
-
-    Returns:
-        dict: A dict with data about votes that the pool received
-    """
-
-    pool_address = Web3.to_checksum_address(pool)
-
-    end_timestamp_votation  = voter_contract.functions.epochVoteEnd(timestamp).call()
-
-    end_block_week = get_block_number_by_timestamp(end_timestamp_votation, 'before')
-
-    try:
-        pool_votes = voter_contract.functions.weights(pool_address).call(block_identifier=end_block_week) / 10**18 # pode dar erro caso coloque uma semana em que essa pool não podia receber votação
-    except:
-        pool_votes = 'ERROR'
-
-    try:
-        total_votes = voter_contract.functions.totalWeight().call(block_identifier=end_block_week) / 10**18
-    except:
-        total_votes = 'ERROR'
-
-    share = pool_votes / total_votes
-        
-    return {'Pool Address': pool, 'Pool Votes': pool_votes, 'Share': share, 'Total Votes': total_votes, 'Timestamp': timestamp}
-
-
-def get_all_votes_for_all_pools_in_the_week(voter_contract, timestamp):
-    """_summary_
-
-    Args:
-        timestamp (int): timestamp choosed by user
-
-    Yields:
-        dict: A dict with data about votes that the pool received
-    """
-
-    block_number = get_block_number_by_timestamp(timestamp, 'before')
-
-    length = voter_contract.functions.length().call(block_identifier=block_number)
-
-    for i in range(0, length):
-        pool_address = voter_contract.functions.pools(i).call(block_identifier=block_number)
-        pool_votes_info = get_pool_votes_in_a_timestamp(pool_address, timestamp)
-        yield pool_votes_info
-
-
     
-''' BRIBES '''
+
 def get_bribes_created(scan_api:ScanApi, pool_contract_address, voter_contract_address='0x16613524e02ad97eDfeF371bC883F2F5d6C480A5', start_block=0, end_block=99999999):
     pool_contract_address = Web3.to_checksum_address(pool_contract_address)
     bribe_pool_list = []
@@ -120,3 +56,4 @@ def get_all_bribes_for_all_pools(scan_api:ScanApi, start_date, end_date, voter_c
         pool_address = voter_contract.functions.pools(i).call()
         bribe_pool_list = get_bribes_created(scan_api, pool_address, voter_contract_address, start_block=start_block, end_block=end_block)
         yield bribe_pool_list
+
--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 from functions_aerodrome import get_all_votes_for_all_pools_in_the_week, get_pool_votes_in_a_timestamp
+from bribes import get_all_bribes_for_all_pools
+from ScanApi.scan_api import ScanApi
+from info import base, API_KEY_BASE_SCAN
+from web3 import Web3
 
-print(get_pool_votes_in_a_timestamp('pool address', timestamp=''))
+# print(get_pool_votes_in_a_timestamp('pool address', timestamp=''))
+
+base_scan = ScanApi(rpc=base, chain='BASE', api_key=API_KEY_BASE_SCAN)
+
+voter_aerodrome_address = Web3.to_checksum_address('0x16613524e02ad97eDfeF371bC883F2F5d6C480A5')
+voter_aerodrome_abi = base_scan.get_contract_abi('0x16613524e02ad97eDfeF371bC883F2F5d6C480A5')
+voter_aerodrome_contract = base_scan.rpc.eth.contract(address=voter_aerodrome_address, abi=voter_aerodrome_abi)
+
+
+for bribe in get_all_bribes_for_all_pools(base_scan, '2024-05-01 10:00:00', '2024-05-07 10:00:00', voter_contract_address='0x16613524e02ad97eDfeF371bC883F2F5d6C480A5'):
+    print(bribe)
